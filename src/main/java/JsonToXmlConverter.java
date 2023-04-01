ecfce86482318dccf4bb6f294749531193044a02
elisaveta9 <lizashishova9@gmail.com>
2023-04-01 21:55:29 +0300

    Update files

--- src/main/java/JsonToXmlConverter.java
+++ src/main/java/JsonToXmlConverter.java
@@ -7,10 +7,10 @@ import java.io.IOException;
 public class JsonToXmlConverter {
     public static void main(String[] args) throws IOException, ParserConfigurationException, TransformerException {
         if (args.length != 1)
-            System.out.println("Некорректное количество аргументов");
+            System.out.println("Incorrect number of arguments");
 
         String outputFile = args[0];
         JsonToXml.Convert(outputFile);
-        System.out.printf("Файл film.xml преобразован в %s", outputFile);
+        System.out.printf("The file film.xml has been converted to a file %s", outputFile);
     }
 }
--- src/main/java/XmlToJsonConverter.java
+++ src/main/java/XmlToJsonConverter.java
@@ -7,10 +7,10 @@ import java.io.IOException;
 public class XmlToJsonConverter {
     public static void main(String[] args) throws  IOException, ParserConfigurationException, SAXException {
         if (args.length != 1)
-            System.out.println("Некорректное количество аргументов");
+            System.out.println("Incorrect number of arguments");
 
         String outputFile = args[0];
         XmlToJson.Convert(outputFile);
-        System.out.printf("Файл film.xml преобразован в %s", outputFile);
+        System.out.printf("The file film.xml has been converted to a file %s", outputFile);
     }
 }
